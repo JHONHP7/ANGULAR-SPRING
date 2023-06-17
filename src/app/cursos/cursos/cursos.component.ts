ff3ecf7c73ba24427157bc93ac8065fb3f86ac87
Jhonatan Silva <jhonatansilva@id.uff.br>
2023-06-16 22:08:33 -0300

    Front 90% pronto

--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { Curso } from '../model/curso';
+import { CursosService } from '../services/cursos.service';
+import { Observable, catchError, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-cursos',
@@ -8,10 +12,27 @@ import { Curso } from '../model/curso';
 })
 export class CursosComponent {
 
-  cursos: Curso[] = [
-    {_id: '1', name: 'Angular', category: 'Front-end'}
-  ];
+  cursos$: Observable <Curso[]> ;
 
   displayedColumns = ['name','category']
 
+  //cursosService  : CursosService;
+
+  constructor(
+    public dialog: MatDialog,
+    private cursosService  : CursosService ){
+    //this.cursosService = new CursosService();
+    this.cursos$ = this.cursosService.list()
+    .pipe( catchError(error =>{
+      this.onError('Erro ao carregar cursos.');
+      return of([])
+    } )
+    );
+  }
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+
+}
 }
---
+++ src/app/cursos/services/cursos.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Curso } from '../model/curso';
+import { delay, first, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  private readonly API = '/assets/cursos.json';
+
+  constructor(private httpClient: HttpClient) { }
+
+  list() {
+    return this.httpClient.get<Curso[]>(this.API).pipe( first(), /* delay(1000), */
+      tap(cursos => console.log(cursos))
+    );
+
+  }
+}
